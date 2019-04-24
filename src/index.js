3fb3468fa5a38192a1bcda2cbd61b6ac5c0b63fa
taihe <taihei618@gmail.com>
2019-04-25 08:15:17 +0900

    first

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost'
+import gql from 'graphql-tag'
+const client = new ApolloClient({
+    uri: 'https://api.github.com/graphql',
+    request: operation => {
+        operation.setContext({ headers: { authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}` } });
+    }
+});
 
+const query = gql`
+{
+    
+}
+`
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
